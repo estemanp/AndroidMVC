0c4ce3e569e7f8a30f221512bc0cfb1127d1d88c
estemanp <estemanp@gmail.com>
2017-09-03 18:38:10 -0500

    GRAL: se borra codigo que no se necesita con un manejador de base de datos

--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
@@ -26,21 +26,8 @@ public class ApplicationController extends Application {
 
     public  ApplicationController(){
         users=new ArrayList<User>();
-        //fillDateBases();
     }
 
-    private void fillDateBases(){
-        //Add users
-        fillUsers();
-    }
-
-    private void fillUsers(){
-        User person1=new User("Andres","Perez","estemanp","1234",24);
-        users.add(person1);
-
-        User person2=new User("Jessica","Ramirez","jessica","1234",16);
-        users.add(person2);
-    }
 
     private boolean isValidUser(String username, String password){
         Boolean resp=false;
